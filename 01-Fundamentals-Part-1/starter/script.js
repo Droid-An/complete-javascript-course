9ff7cef202030d61f4fa4e682e44937db215e7da
Droid-An <andrmail6@gmail.com>
2025-05-05 12:08:22 +0100

    first part

---
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -0,0 +1,29 @@
+// const bill = 275;
+const bill = 40;
+// const bill = 430
+const tip = bill >= 50 && bill <= 300 ? bill * 0.15 : bill * 0.2;
+console.log(
+  `The bill was ${bill}, the tip was ${tip}, and the total value ${bill + tip}`
+);
+const lang = "Spanish";
+
+switch (lang) {
+  case "Mandarin":
+  case "Chinese":
+    console.log("MOST number of native speakers!");
+    break;
+  case "Spanish":
+    console.log("2nd place in number of native speakers");
+    break;
+  case "English":
+    console.log("3rd place");
+    break;
+  case "Hindi":
+    console.log("Number 4");
+    break;
+  case "Arabic":
+    console.log("5th most spoken language");
+    break;
+  default:
+    console.log("Great language too :D");
+}
